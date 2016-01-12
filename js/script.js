d161a502b2618bda19b81006a2621749108e223d
agreg0ire <artgreg@outlook.fr>
2016-01-12 11:51:17 +0100

    - added func_num_Args for less empiricity

--- js/script.js
+++ js/script.js
@@ -14,6 +14,8 @@ var styleTampoonContainer = 'border: 2px solid cornflowerblue;border-radius: 3px
 function makeSum(p1_input_number)
 {
     sum = 0;
+    
+    console.log(func_num_args());
 
     if(typeof p1_input_number === 'object')
     {
@@ -445,4 +447,23 @@ function array_rand(input, num_req) {
     }
 
     return ((ticks == 1) ? indexes.join() : indexes);
+}
+
+function func_num_args () {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Brett Zamir (http://brett-zamir.me)
+    // %        note 1: May not work in all JS implementations
+    // *     example 1: function tmp_a () {return func_num_args();}
+    // *     example 1: tmp_a('a', 'b');
+    // *     returns 1: 2
+    if (!arguments.callee.caller) {
+        try {
+            throw new Error('Either you are using this in a browser which does not support the "caller" property or you are calling this from a global context');
+            //return false;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    return arguments.callee.caller.arguments.length;
 }
