831d97f46c32fecbccc35a86c4283a261f740f6c
suzon007 <sujon.zahid@yahoo.com>
2016-05-03 13:02:28 +0600

    fix the jQuery problem

--- js/responsive-tgl.js
+++ js/responsive-tgl.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
+  var display_size = $( window ).width();
+  if(display_size <= 768){
     $( "#toggle-div" ).hide();
-
+  }
     $(".tgl-button").click(function(){
         $("#toggle-div").slideToggle();
     });
